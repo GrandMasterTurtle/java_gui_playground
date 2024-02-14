71ba19da64f1c8835d2763347b143589f39cc0f8
Excalibur L. Bunker <Excalibur.l.bunker@gmail.com>
2024-02-14 15:49:38 -0600

    Added Icon to TeamPanel.java

--- TeamPanel.java
+++ TeamPanel.java
@@ -8,13 +8,20 @@ public class TeamPanel extends JPanel {
 
 
     public TeamPanel() {
+
+        ImageIcon imageIcon = new ImageIcon("3135715.png");
+        Image image = imageIcon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(240, 240,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        imageIcon = new ImageIcon(newimg);  // transform it back
+
         memberLabels = new JLabel[TEAM_SIZE];
 
         // create your own label that have an image and your name as text.
         memberLabels[0] = new JLabel("Sample", new ImageIcon("smile.png"), SwingConstants.CENTER);
         memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[1] = new JLabel("Excalibur", new ImageIcon("3135715.png"), SwingConstants.RIGHT);
+
+        memberLabels[1] = new JLabel("Excalibur", imageIcon, SwingConstants.CENTER);
         memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[1].setVerticalTextPosition(SwingConstants.BOTTOM);
         memberLabels[2] = new JLabel();
@@ -25,7 +32,7 @@ public class TeamPanel extends JPanel {
             this.add(member);
         }
 
-        this.setPreferredSize(new Dimension(500, 250));
+        this.setPreferredSize(new Dimension(500, 500));
         this.setBackground(Color.LIGHT_GRAY);
 
 
