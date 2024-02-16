2e40a11ac894b378a685cfcf6a21db9abdfcd091
Jeremiah-GJ <104379421+Jeremiah-GJ@users.noreply.github.com>
2024-02-16 08:51:57 -0600

    Added my portion

--- TeamPanel.java
+++ TeamPanel.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class TeamPanel extends JPanel {
 
-    private final int TEAM_SIZE = 3;
+    private final int TEAM_SIZE = 4;
     private JLabel[] memberLabels;
 
 
@@ -15,15 +15,15 @@ public class TeamPanel extends JPanel {
         memberLabels[0].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[0].setVerticalTextPosition(SwingConstants.BOTTOM);
 
-        memberLabels[1] = new JLabel("Excalibur", imageIcon, SwingConstants.CENTER);
+        memberLabels[1] = new JLabel("Excalibur", new ImageIcon("3135715.png"), SwingConstants.CENTER);
         memberLabels[1].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[1].setVerticalTextPosition(SwingConstants.BOTTOM);
-        memberLabels[2] = new JLabel();
-        memberLabels[1] = new JLabel();
+        memberLabels[3] = new JLabel("Jeremiah",new ImageIcon("mark,png"),SwingConstants.LEFT );
         memberLabels[2] = new JLabel("Kevin", new ImageIcon("insect.png"), SwingConstants.RIGHT);
         memberLabels[2].setHorizontalTextPosition(SwingConstants.CENTER);
         memberLabels[2].setVerticalTextPosition(SwingConstants.BOTTOM);
-
+        memberLabels[3].setHorizontalTextPosition(SwingConstants.CENTER);
+        memberLabels[3].setVerticalTextPosition(SwingConstants.BOTTOM);
 
 
         for (JLabel member : memberLabels) {
